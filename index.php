1748f8e6114ddba39e19c3a3baae8a7d56300766
valenciaxf <valencia.xf@gmail.com>
2026-01-23 20:04:32 -0600

    updateJqueryVersion23012026

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ header("location: myWalletX.php");
 <head>
   <title>Login MyWalletX</title>
 
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" />
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <link href="bootstrap/bootstrap-4.4.1-dist/css/bootstrap.min.css" rel="stylesheet" />
+  <script src="bootstrap/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
 
   <script language="javascript">
     window.setTimeout(function () {
